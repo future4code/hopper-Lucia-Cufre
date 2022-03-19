753ce3c0b0a48ffe08d79e93f4a41577ccca91aa
lucia-cufre <luchicufre@hotmail.com>
2022-03-18 23:32:25 -0300

    ..

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -5,8 +5,8 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   array 
-   return array.length
+  array
+  return array.length
 }
 
 // EXERCÍCIO 02
@@ -18,16 +18,16 @@ function retornaArrayInvertido(array) {
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
   array
-  return array.sort(function compararNumeros(a,b){
-      return a - b
+  return array.sort(function compararNumeros(a, b) {
+    return a - b
   })
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
   array
-  let pares = function(numero){
-      return numero % 2 == 0
+  let pares = function (numero) {
+    return numero % 2 == 0
   }
   let numeroPar = array.filter(pares)
 
@@ -36,19 +36,19 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-   // map e filter
+  
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
   array
   let maior = -Infinity
-for (let numero of array){
-    if(numero > maior){
-        maior = numero
+  for (let numero of array) {
+    if (numero > maior) {
+      maior = numero
     }
-}
-return maior
+  }
+  return maior
 }
 
 // EXERCÍCIO 07
@@ -56,48 +56,48 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
   let maior
   let menor
   let divisivel
-  if (num1 > num2){
+  if (num1 > num2) {
     maior = num1
-  }else{
+  } else {
     maior = num2
   }
-  if (num1 < num2){
+  if (num1 < num2) {
     menor = num1
-  }else{
+  } else {
     menor = num2
   }
-  if (maior % menor === 0){
+  if (maior % menor === 0) {
     divisivel = true
-  }else{
+  } else {
     divisivel = false
   }
   let diferenca = maior - menor
-const objeto = {
-  maiorNumero: maior,
-  maiorDivisivelPorMenor: divisivel,
-  diferenca: diferenca
-}
+  const objeto = {
+    maiorNumero: maior,
+    maiorDivisivelPorMenor: divisivel,
+    diferenca: diferenca
+  }
   return objeto
-  
+
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
   let numerosPares = []
-  for(let i = 0; numerosPares.length < n; i += 2){
-   numerosPares.push(i)
-  } 
+  for (let i = 0; numerosPares.length < n; i += 2) {
+    numerosPares.push(i)
+  }
   return numerosPares
 
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-  if(ladoA == ladoB && ladoB == ladoC){
+  if (ladoA == ladoB && ladoB == ladoC) {
     return "Equilátero"
-  }else if(ladoA == ladoB || ladoA == ladoC || ladoB == ladoC){
+  } else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
     return "Isósceles"
-  }else{
+  } else {
     return "Escaleno"
   }
 }
@@ -113,54 +113,76 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   let objeto = {
-      nome: 'O Diabo Veste Prada',
-      ano: 2006,
-      diretor: 'David Frankel',
-      atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
-   }
-   filme = objeto
-   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+  let objeto = {
+    nome: 'O Diabo Veste Prada',
+    ano: 2006,
+    diretor: 'David Frankel',
+    atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+  }
+  filme = objeto
+  return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   return {...pessoa,
-  nome: "ANÔNIMO"}
+  return {
+    ...pessoa,
+    nome: "ANÔNIMO"
+  }
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
   const pessoasAutorizadas = []
-  for(pessoa of pessoas){
-    if(pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60){
-    pessoasAutorizadas.push(pessoa)}
+  for (pessoa of pessoas) {
+    if (pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60) {
+      pessoasAutorizadas.push(pessoa)
+    }
   }
   return pessoasAutorizadas
-  
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
   const pessoasNaoAutorizadas = []
-  for (pessoa of pessoas){
-    if(pessoa.altura <= 1.4 || pessoa.idade <= 14 || pessoa.idade >= 60){
-    pessoasNaoAutorizadas.push(pessoa)}
+  for (pessoa of pessoas) {
+    if (pessoa.altura <= 1.4 || pessoa.idade <= 14 || pessoa.idade >= 60) {
+      pessoasNaoAutorizadas.push(pessoa)
+    }
   }
   return pessoasNaoAutorizadas
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
+  contas.map(function (elemento, index) {
+    let totalCompras = contas[index].compras.reduce(function (valorInicial, valorAtual) {
+      return valorInicial + valorAtual
+    }, 0)
+    contas[index].saldoTotal = contas[index].saldoTotal - totalCompras
+    contas[index].compras = []
+  })
+  return contas
 }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+  consultas.sort(function (a, b) {
+    if (a.nome > b.nome) {
+      return 1;
+    }
+    if (a.nome < b.nome) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+  return consultas
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
