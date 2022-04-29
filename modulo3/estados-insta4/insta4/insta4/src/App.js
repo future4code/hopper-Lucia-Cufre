100569ecd2116529768285099f21f17977e1333b
lucia-cufre <luchicufre@hotmail.com>
2022-04-29 19:05:43 -0300

    solucao dos erros

--- modulo3/estados-insta4/insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/insta4/src/App.js
@@ -1,101 +1,105 @@
-import React from 'react';
-import styled from 'styled-components'
-import Post from './components/Post/Post';
+import React from "react";
+import styled from "styled-components";
+import Post from "./components/Post/Post";
 
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 class App extends React.Component {
-
   state = {
     post: [
-    {
-      nomeUsuario: "paulinha",
-      fotoUsuario:"https://picsum.photos/50/50",
-      fotoPrincipal:"https://picsum.photos/200/150"
-    },
-    {
-      nomeUsuario: "luli",
-      fotoUsuario:"https://picsum.photos/50/60",
-      fotoPrincipal:"https://picsum.photos/200/160"
-    },
-    {
-      nomeUsuario: "lorena",
-      fotoUsuario:"https://picsum.photos/50/70",
-      fotoPrincipal:`https://picsum.photos/200/170`
-    },
+      {
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150",
+      },
+      {
+        nomeUsuario: "luli",
+        fotoUsuario: "https://picsum.photos/50/60",
+        fotoPost: "https://picsum.photos/200/160",
+      },
+      {
+        nomeUsuario: "lorena",
+        fotoUsuario: "https://picsum.photos/50/70",
+        fotoPost: `https://picsum.photos/200/170`,
+      },
+    ],
 
-  ],
+    inputNomeUsuario: "",
+    inputFotoUsuario: "",
+    inputFotoPrincipal: "",
+  };
 
-  inputNomeUsuario:"",
-  inputFotoUsuario:"",
-  inputFotoPrincipal:""
+  adicionaPost = () => {
+    const postNovo = {
+      nomeUsuario: this.state.inputNomeUsuario,
+      fotoUsuario: this.state.inputFotoUsuario,
+      fotoPost: this.state.inputFotoPrincipal,
+    };
 
-};
+    const novaPubli = [...this.state.post, postNovo];
 
-adicionaPost = () => {
-const postNovo = {
-  nomeUsuario: this.state.inputNomeUsuario,
-      fotoUsuario:this.state.inputFotoUsuario,
-      fotoPrincipal:this.state.inputFotoPrincipal
-};
+    this.setState({ post: novaPubli });
+    this.setState({
+      inputNomeUsuario: "",
+      inputFotoUsuario: "",
+      inputFotoPrincipal: "",
+    });
+  };
 
-const novaPubli = [...this.state.post, postNovo]
-
-this.setState({ post : novaPubli})
-this.setState({inputNomeUsuario: "", inputFotoUsuario:"", inputFotoPrincipal: ""})
-
-}
-
-onChangeNomeUsuario = (event) =>{
-  this.setState({inputNomeUsuario : event.target.value})
- }
- onChangeFotoUsuario = (event) =>{
-   this.setState({inputFotoUsuario : event.target.value})
-  }
-  onChangeFotoPrincipal = (event) =>{
-   this.setState({inputFotoPrincipal : event.target.value})
-  }
-
-
+  onChangeNomeUsuario = (event) => {
+    this.setState({ inputNomeUsuario: event.target.value });
+  };
+  onChangeFotoUsuario = (event) => {
+    this.setState({ inputFotoUsuario: event.target.value });
+  };
+  onChangeFotoPrincipal = (event) => {
+    this.setState({ inputFotoPrincipal: event.target.value });
+  };
 
   render() {
-
-    const publis = this.state.post.map((posts) => {
+    const listaComponentes = this.state.post.map((posts, index) => {
       return (
-        <p>{posts.nomeUsuario}</p>,
-        <img >{posts.fotoUsuario}</img>,
-        <a url={posts.fotoPrincipal}/> 
-        /* nao entendi como colocar a imagem para ser visualizada  */
-        );
-      });
+        <Post
+          nomeUsuario={posts.nomeUsuario}
+          fotoUsuario={posts.fotoUsuario}
+          fotoPost={posts.fotoPost}
+          key={index}
+        />
+      );
+    });
 
-      
     return (
       <MainContainer>
         <div>
-       <input type= "nome" value={this.state.inputNomeUsuario} onChange={this.onChangeNomeUsuario} placeHolder={"Nome de Usuario"}/>
-       <input type = "url" value={this.state.inputFotoUsuario} onChange={this.onChangeFotoUsuario} placeHolder={"Foto de Usuario"}/>
-       <input type = "url" value={this.state.inputFotoPrincipal} onChange={this.onChangeFotoPrincipal} placeHolder={"Foto de publicacao"}/>
-       <button onClick={this.adicionaPost}>Publicar</button>
+          <input
+            type="nome"
+            value={this.state.inputNomeUsuario}
+            onChange={this.onChangeNomeUsuario}
+            placeholder={"Nome de Usuario"}
+          />
+          <input
+            type="url"
+            value={this.state.inputFotoUsuario}
+            onChange={this.onChangeFotoUsuario}
+            placeholder={"Foto de Usuario"}
+          />
+          <input
+            type="url"
+            value={this.state.inputFotoPrincipal}
+            onChange={this.onChangeFotoPrincipal}
+            placeholder={"Foto de publicacao"}
+          />
+          <button onClick={this.adicionaPost}>Publicar</button>
         </div>
-        <Post
-          {...publis}
-        />
-        <Post
-          {...publis}
-        />
-        <Post
-          {...publis}
-        />
-        
-      </MainContainer> 
+        <div>{listaComponentes}</div>
+      </MainContainer>
     );
   }
-  }
+}
 
 export default App;
