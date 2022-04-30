aa9d940e4052d4aeadc8c2b8d9c0a50b2768bfc2
lucia-cufre <luchicufre@hotmail.com>
2022-04-29 23:28:59 -0300

    progressando

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  state = {
+    mensagens: [
+      {
+        usuario: "",
+        conteudo: "",
+      },
+    ],
+
+    valorInputUsuario: "",
+    valorInputConteudo: "",
+  };
+
+  enviaMensagem = () => {
+    const novaMensagem = {
+      usuario: this.state.valorInputUsuario,
+      conteudo: this.state.valorInputConteudo,
+    };
+
+    const novasMensagens = [...this.state.mensagens, novaMensagem];
+
+    this.setState({
+      mensagens: novasMensagens,
+      valorInputUsuario: "",
+      valorInputConteudo: "",
+    });
+  };
+
+  onChangeUsuario = (event) => {
+    this.setState({ valorInputUsuario: event.target.value });
+  };
+
+  onChangeConteudo = (event) => {
+    this.setState({ valorInputConteudo: event.target.value });
+  };
+
+  render() {
+    const listaDeMensagens = this.state.mensagens.map((mensagem, index) => {
+      return (
+        <p key={index}>
+          {mensagem.usuario} : {mensagem.conteudo}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      );
+    });
+
+    return (
+      <div>
+        <div>{listaDeMensagens}</div>
+
+        <div>
+          <input
+            value={this.state.valorInputUsuario}
+            onChange={this.onChangeUsuario}
+            placeholder={"Usuario"}
+          />
+          <input
+            value={this.state.valorInputConteudo}
+            onChange={this.onChangeConteudo}
+            placeholder={"Mensagem"}
+          />
+
+          <button onClick={this.enviaMensagem}>Enviar</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ modulo3/projeto-whatslab/whatslab/src/Components/ListaDeMensagem.js
@@ -0,0 +1,40 @@
+/* import React from "react";
+
+class ListaDeMensagem extends React.Component {
+  state = {
+    mensagens: [
+      {
+        usuario: "",
+        conteudo: "",
+      },
+    ],
+  }; */
+
+  /* enviaMensagem = () => {
+
+const novaMensagen = {
+usuario: this.state.mensagens.usuario,
+conteudo: this.state.mensagens.conteudo
+};
+
+const novasMensagens = [...this.state.mensagens, novaMensagen]
+
+this.setState({mensagens:novasMensagens})
+this.setState({usuario: "", conteudo:""})
+
+} */
+/* 
+  render() {
+    const listaDeMensagens = this.state.mensagens.map((mensagen, index) => {
+      return (
+        <p>
+          '${mensagen.usuario}: ${mensagen.conteudo}'
+        </p>
+      );
+    });
+
+    return <div>{listaDeMensagens}</div>;
+  }
+}
+
+export default ListaDeMensagem; */
