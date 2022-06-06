06ad9e63914370055b24060d4f55960f2e7f0365
lucia-cufre <luchicufre@hotmail.com>
2022-06-06 18:38:57 -0300

    projeto

--- modulo4/projeto-astromatch/astro-match/src/Pages/TelaInicial.js
+++ modulo4/projeto-astromatch/astro-match/src/Pages/TelaInicial.js
@@ -1,6 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Clear from "../Component/ButtonClear.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+*{
+  box-sizing: boder-box;
+}
+
+`
+const Card = styled.div`
+width: 400px;
+height: 600px;
+top: 50%;
+left: 50%;
+border: 1px solid black;
+border-radius: 5px;
+background-color: white;
+box-shadow: rgb(0 0 0 / 6%) 0px 0px 5px;
+
+` 
+const Inside = styled.div`
+display: flex;
+flex-direction:column;
+height: 100%;
+
+`
+const Top = styled.div`
+height: 50px;
+border-bottom: 1px solid #d3d3d3;
+
+`
+const Img = styled.img`
+width: 20vw;
+
+`
 
 function Home(props) {
   const [profiles, setProfiles] = useState([]);
@@ -55,18 +89,33 @@ function Home(props) {
  
 
   return (
-    <div>
+    <Container>
+      <Card>
+      <Inside>
+        <Top>
+          <h2>astromatch</h2>
       <button onClick={props.onClick}>MATCHES</button>
+      </Top>
       <div>
-        <img src={profiles.photo} alt={profiles.photo_alt} />
+        
+        <Img src={profiles.photo} alt={profiles.photo_alt} />
+        
+        <div>
         <p>{profiles.name}</p>
         <p>{profiles.age}</p>
         <p>{profiles.bio}</p>
-      </div>
-      <button onClick={choosePerson}>NAO</button>
+        </div>
+        <div>
+    <button onClick={choosePerson}>NAO</button>
       <button onClick={choosePerson}>SIM</button>
+      </div>
+      </div>
+      </Inside>
+      </Card>
+      <footer>
       <Clear/>
-    </div>
+      </footer>
+    </Container>
   );
 }
 
