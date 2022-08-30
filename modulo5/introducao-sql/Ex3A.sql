b8d05e7fb7254278f66bed3bf1c022bb19f3ed4b
Lucia Cufre Aman <luchicufre@hotmail.com>
2022-08-30 20:38:04 -0300

    intro-sql

---
+++ modulo5/introducao-sql/Ex3A.sql
@@ -0,0 +1,3 @@
+use `Hopper-4314048-lucia-aman`;
+
+select * from Funcionarios;
---
+++ modulo5/introducao-sql/Ex3D.sql
@@ -0,0 +1,4 @@
+use `Hopper-4314048-lucia-aman`;
+
+select  id as identifier, name, email from Funcionarios
+ where name like "%a%";
